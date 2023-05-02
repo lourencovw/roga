3bf46207ea76dfd9e0df7c282af37cc4b724b1a4
Lourenco Werneck <lourencovw@gmail.com>
2023-05-02 16:04:39 -0300

    Add routes

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,9 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.resource('people', 'UsersController').middleware('auth')
+Route.resource('notes', 'NotesController').middleware('auth')
+
+Route.post('login', 'UsersController.login')
+Route.post('register', 'UsersController.register')
+
