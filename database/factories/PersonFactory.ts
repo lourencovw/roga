e22e4628eadb16d40899c4cd39c077e94989e95e
Lourenco Werneck <lourencovw@gmail.com>
2023-05-02 16:05:15 -0300

    Factories

---
+++ database/factories/PersonFactory.ts
@@ -0,0 +1,8 @@
+import Person from 'App/Models/Person'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(Person, ({ faker }) => {
+  return {
+    //
+  }
+}).build()
