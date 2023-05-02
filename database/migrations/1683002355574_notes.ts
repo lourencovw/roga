832d7061fd734f0ab6109ef60ed71884fefcf4c1
Lourenco Werneck <lourencovw@gmail.com>
2023-05-02 16:05:03 -0300

    Migrations

---
+++ database/migrations/1683002355574_notes.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'notes'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('person_id')
+        .unsigned()
+        .references('id')
+        .inTable('users').onDelete('CASCADE')
+      table.string('title').notNullable()
+      table.string('description', 2000)
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
