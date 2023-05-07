170bd75c22269b9520b26416f6b24f61a2668a85
Lourenco Werneck <lourencovw@gmail.com>
2023-05-07 00:26:03 -0300

    Person factory

--- database/factories/PersonFactory.ts
+++ database/factories/PersonFactory.ts
@@ -1,8 +1,17 @@
 import Person from 'App/Models/Person'
 import Factory from '@ioc:Adonis/Lucid/Factory'
+import { DateTime } from 'luxon'
 
 export default Factory.define(Person, ({ faker }) => {
+  const randomDate = {
+    days: faker.datatype.number({ min: 0, max:29200 })
+  }
+
   return {
-    //
+    name: faker.name.fullName(),
+    mother_name: faker.name.fullName(),
+    father_name: faker.name.fullName(),
+    cep: faker.address.zipCode('########') ,
+    birthdate: DateTime.now().minus(randomDate).toISODate()  ,
   }
 }).build()
