a08c36b39633dd0175296d6ca7e8d551c19fc5e4
Lourenco Werneck <lourencovw@gmail.com>
2023-05-07 00:24:50 -0300

    Add people's controller

--- app/Controllers/Http/PeopleController.ts
+++ app/Controllers/Http/PeopleController.ts
@@ -1,13 +1,45 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Person from 'App/Models/Person'
+import CreatePerson from 'App/Validators/CreatePersonValidator';
 
 export default class PeopleController {
-  public async index({}: HttpContextContract) {}
+  public async index() {
+    return await Person.query().preload('notes')
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request }: HttpContextContract) {
+    const payload = await request.validate(CreatePerson);
 
-  public async show({}: HttpContextContract) {}
+    const person = new Person()
+    person.name = payload.name
+    person.father_name = payload.father_name
+    person.mother_name = payload.mother_name
+    person.cep = payload.cep
+    person.birthdate = payload.birthdate
 
-  public async update({}: HttpContextContract) {}
+    // Insert to the database
+    return await person.save()
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async show({ request }: HttpContextContract) {
+
+    return await Person.query()
+      .where('id', Number(request.param('id')))
+      .preload('notes')
+      .firstOrFail()
+  }
+
+  public async update({ request }: HttpContextContract) {
+    const person = await Person.findOrFail(request.param('id'))
+    return await person
+      .merge(request.body())
+      .save()
+  }
+
+  public async destroy({ request }: HttpContextContract) {
+    const person = await Person.findOrFail(request.param('id'))
+    await person.delete()
+
+    return true
+  }
 }
--- app/Models/Person.ts
+++ app/Models/Person.ts
@@ -1,9 +1,29 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import Note from 'App/Models/Note'
 
 export default class Person extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public father_name: string
+
+  @column()
+  public mother_name: string
+
+  @column()
+  public cep: string
+
+  @hasMany(() => Note)
+  public notes: HasMany<typeof Note>
+
+  @column.date()
+  public birthdate: Date
+
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
